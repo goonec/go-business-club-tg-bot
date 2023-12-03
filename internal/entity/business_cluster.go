9a45bd27ac31fced13de4369afd5a84826807d62
Enthreeka <n3ksmirnov@gmail.com>
2023-12-04 01:56:13 +0300

    feat: new entity

---
+++ internal/entity/business_cluster.go
@@ -0,0 +1,6 @@
+package entity
+
+type BusinessCluster struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
---
+++ internal/entity/business_cluster_resident.go
@@ -0,0 +1,6 @@
+package entity
+
+type BusinessClusterResident struct {
+	IDResident int    `json:"resident_id"`
+	Name       string `json:"name"`
+}
